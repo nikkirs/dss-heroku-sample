d4c1182731fc0fd9d376def8c557007271e4fc3c
nikhil yadav <nyadav@manh.com>
2022-01-27 11:51:36 +0530

    Add tracking, curbside, and return

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(__dirname));
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get('/tracking', function (req, res) {
+  res.sendFile(path.join(__dirname + "/tracking.html"));
+});
+
+app.get('/curbside', function (req, res) {
+  res.sendFile(path.join(__dirname + "/curbside.html"));
+});
+
+app.get('/return', function (req, res) {
+  res.sendFile(path.join(__dirname + "/return.html"));
 });
 
 const port = process.env.PORT || '5000';
